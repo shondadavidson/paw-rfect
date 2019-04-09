aa724b8640efdff78dc359b547891365ec4265f3
Shonda Schallenberger <shonda@Shondas-MacBook-Pro.local>
2019-04-09 14:26:36 -0600

    added user_auth table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import ServiceProviderList from './Components/ServiceProviderList/ServiceProviderList'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <h1>Welcome to paw-rfect</h1>
+      <ServiceProviderList />
       </div>
     );
   }
