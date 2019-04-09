3d38a51ada50477c855ce0606a1d3dda93bdfe39
Anthony Viglione <viglione2014.2015@gmail.com>
2019-04-09 16:29:26 -0600

    created basic AddDog component

---
+++ src/Components/AddDog/AddDog.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class AddDog extends Component {
+    constructor(){
+        super()
+
+    }
+
+    render(){
+        return(
+            <>
+
+            </>
+        )
+    }
+}
+
+export default AddDog;
