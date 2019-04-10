59c9575fc4a97445715d11007d87e5364efa1df0
Anthony Viglione <viglione2014.2015@gmail.com>
2019-04-10 10:42:32 -0600

    created basic ProviderProfile component that renders it's name

---
+++ src/Components/ProviderProfile/ProviderProfile.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class ProviderProfile extends Component{
+    constructor(){
+        super()
+
+    }
+
+    render(){
+        return(
+            <>
+                <h3>ProviderProfile Component</h3>
+            </>
+        )
+    }
+}
+
+export default ProviderProfile;
