94737ecaa0b7516cbbf3211c3d8d3a529da80039
Anthony Viglione <viglione2014.2015@gmail.com>
2019-04-10 11:04:03 -0600

    added routes for Splash, OwnerProfile, AddDog, and ProviderProfile -> Changed route for Dashboard from '/' to '/dashboard' and gave Splash route '/'

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Dashboard from '../Components/Dashboard/Dashboard'
-import Home from '../Components/Home/Home'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Dashboard from '../Components/Dashboard/Dashboard';
+import Home from '../Components/Home/Home';
+import Splash from '../Components/Splash/Splash';
+import OwnerProfile from '../Components/OwnerProfile/OwnerProfile';
+import AddDog from '../Components/AddDog/AddDog';
+import ProviderProfile from '../Components/ProviderProfile/ProviderProfile';
 
 export default (
     <Switch>
-        <Route exact path='/' component={Dashboard} />
-        <Route path='/home' component={Home} />
+        <Route exact path='/' component={Splash} />
+        <Route exact path='/dashboard' component={Dashboard} />
+        <Route exact path='/home' component={Home} />
+        <Route exact path='/ownerprofile' component={OwnerProfile} />
+        <Route exact path='/adddog' component={AddDog} />
+        <Route exact path='/providerprofile' component={ProviderProfile} />
+
     </Switch>
 )
