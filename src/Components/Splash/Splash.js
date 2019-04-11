ba03e5b1c8e80015d8b309f1a0ffd0c57a95e74a
Anthony Viglione <viglione2014.2015@gmail.com>
2019-04-11 10:31:00 -0600

    improved format

--- src/Components/Splash/Splash.js
+++ src/Components/Splash/Splash.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function Splash(){
 
@@ -9,7 +9,9 @@ function Splash(){
             <div>Welcome to Paw-rfect</div>
             <div>The Paw-rfect way to find the best care for your good boy(or girl).</div>
             <div>Connecting you to the best local dog walkers, sitters, and boarders.</div>
-              <Link to='/dashboard'><button>Login/Register</button></Link>
+            <Link to='/dashboard'>
+                <button>Login/Register</button>
+            </Link>
             <div>pictures of dogs go here</div>
             </>
         )
