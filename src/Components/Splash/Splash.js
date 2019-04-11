25f22ca48b49f05e57d191cabca577cc089ca938
Anthony Viglione <viglione2014.2015@gmail.com>
2019-04-11 09:47:57 -0600

    converted component back to functional

--- src/Components/Splash/Splash.js
+++ src/Components/Splash/Splash.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 
-class Splash extends Component {
+function Splash(){
 
-    render() {
+
         return(
             <>
             <div>Welcome to Paw-rfect</div>
             <div>The Paw-rfect way to find the best care for your good boy(or girl).</div>
-            <div>Connecting you to the best local dog walkers, sitters, an d boarders.</div>
+            <div>Connecting you to the best local dog walkers, sitters, and boarders.</div>
               <Link to='/dashboard'><button>Login/Register</button></Link>
             <div>pictures of dogs go here</div>
             </>
         )
-    }
 }
 
 export default Splash;
