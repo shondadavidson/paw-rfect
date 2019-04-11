4288aabe522808abd6c81e75aa04e00c924d9647
Diwash Sigdel <priyankasharma@Priyankas-MacBook-Air.local>
2019-04-11 17:09:10 -0600

    merging

--- server/index.js
+++ server/index.js
@@ -1,89 +1,95 @@
-const path = require('path');
-require('dotenv').config()
-const express = require ('express')
-const session = require ('express-session')
-const massive = require ('massive')
-const pg = require ('pg')
-const pgSession = require('connect-pg-simple')(session)
+const path = require("path");
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const massive = require("massive");
+const pg = require("pg");
+const pgSession = require("connect-pg-simple")(session);
 
-const ac = require('./controllers/auth_controller')
-const uc = require('./controllers/user_controller')
+const ac = require("./controllers/auth_controller");
+const uc = require("./controllers/user_controller");
 
-const app = express()
-const aws = require('aws-sdk');
+const app = express();
+const aws = require("aws-sdk");
 
-const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET, S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env
-
-
+const {
+  SERVER_PORT,
+  CONNECTION_STRING,
+  SESSION_SECRET,
+  S3_BUCKET,
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY
+} = process.env;
 
 const pgPool = new pg.Pool({
-    connectionString: CONNECTION_STRING
-  })
+  connectionString: CONNECTION_STRING
+});
 
-  app.use(express.json())
-app.use(session({
+app.use(express.json());
+app.use(
+  session({
     store: new pgSession({
-        pool: pgPool,
-        pruneSessionInterval: 60 * 60 * 24
+      pool: pgPool,
+      pruneSessionInterval: 60 * 60 * 24
     }),
     secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        maxAge: 1231232223211
+      maxAge: 1231232223211
     }
-}))
+  })
+);
 
 massive(CONNECTION_STRING).then(db => {
-    app.set('db', db)
-    app.listen(SERVER_PORT, () => console.log('Sweeettt'))
-})
+  app.set("db", db);
+  app.listen(SERVER_PORT, () => console.log("Sweeettt"));
+});
 
 //Amazon s-3
 
-app.get('/api/signs3', (req, res) => {
-    aws.config = {
-      region: 'us-west-1',
-      accessKeyId: AWS_ACCESS_KEY_ID,
-      secretAccessKey: AWS_SECRET_ACCESS_KEY,
+app.get("/api/signs3", (req, res) => {
+  aws.config = {
+    region: "us-west-1",
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY
+  };
+
+  const s3 = new aws.S3();
+  const fileName = req.query["file-name"];
+  const fileType = req.query["file-type"];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: "public-read"
+  };
+
+  s3.getSignedUrl("putObject", s3Params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
     };
-  
-    const s3 = new aws.S3();
-    const fileName = req.query['file-name'];
-    const fileType = req.query['file-type'];
-    const s3Params = {
-      Bucket: S3_BUCKET,
-      Key: fileName,
-      Expires: 60,
-      ContentType: fileType,
-      ACL: 'public-read',
-    };
-  
-    s3.getSignedUrl('putObject', s3Params, (err, data) => {
-      if (err) {
-        console.log(err);
-        return res.end();
-      }
-      const returnData = {
-        signedRequest: data,
-        url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
-      };
-  
-      return res.send(returnData);
-    });
+    console.log(returnData)
+    return res.send(returnData);
   });
-  
+});
 
 //Auth_controller
-app.post('/auth/register', ac.register)
-app.post('/auth/login', ac.login)
-app.post('/auth/logout', ac.logout)
+app.post("/auth/register", ac.register);
+app.post("/auth/login", ac.login);
+app.post("/auth/logout", ac.logout);
 
-app.get('/api/current', ac.getUser)
+app.get("/api/current", ac.getUser);
 
 //user_controller
 
-app.get('/api/getMyProviders/:id', uc.getMyProviders)
-app.post('/api/provider/:id', uc.provider)
-app.get('/api/searchProviders/:zip', uc.searchProviders)
-app.post('/api/addProvider/:id', uc.addProvider)
+app.get("/api/getMyProviders/:id", uc.getMyProviders);
+app.post("/api/provider/:id", uc.provider);
+app.get("/api/searchProviders/:zip", uc.searchProviders);
+app.post("/api/addProvider/:id", uc.addProvider);
--- src/Components/ImageUpload/ImageUpload.js
+++ src/Components/ImageUpload/ImageUpload.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { v4 as randomString } from 'uuid';
-import Dropzone from 'react-dropzone';
-import { GridLoader } from 'react-spinners';
+import React, { Component } from "react";
+import axios from "axios";
+import { v4 as randomString } from "uuid";
+import Dropzone from "react-dropzone";
+import { GridLoader } from "react-spinners";
 
 class ImageUpload extends Component {
   constructor() {
     super();
     this.state = {
       isUploading: false,
-      url: 'http://via.placeholder.com/450x450',
+      url: "http://via.placeholder.com/450x450"
     };
   }
 
   getSignedRequest = ([file]) => {
     this.setState({ isUploading: true });
     // We are creating a file name that consists of a random string, and the name of the file that was just uploaded with the spaces removed and hyphens inserted instead. This is done using the .replace function with a specific regular expression. This will ensure that each file uploaded has a unique name which will prevent files from overwriting other files due to duplicate names.
-    const fileName = `${randomString()}-${file.name.replace(/\s/g, '-')}`;
+    const fileName = `${randomString()}-${file.name.replace(/\s/g, "-")}`;
 
     // We will now send a request to our server to get a "signed url" from Amazon. We are essentially letting AWS know that we are going to upload a file soon. We are only sending the file-name and file-type as strings. We are not sending the file itself at this point.
     axios
-      .get('/api/signs3', {
+      .get("/api/signs3", {
         params: {
-          'file-name': fileName,
-          'file-type': file.type,
-        },
+          "file-name": fileName,
+          "file-type": file.type
+        }
       })
       .then(response => {
         const { signedRequest, url } = response.data;
@@ -36,23 +36,25 @@ class ImageUpload extends Component {
   };
 
   uploadFile = (file, signedRequest, url) => {
+    console.log(file, signedRequest);
     const options = {
       headers: {
-        'Content-Type': file.type,
-      },
+        "Content-Type": file.type
+      }
     };
 
     axios
       .put(signedRequest, file, options)
       .then(response => {
+        console.log(response);
         this.setState({ isUploading: false, url });
         // THEN DO SOMETHING WITH THE URL. SEND TO DB USING POST REQUEST OR SOMETHING
       })
       .catch(err => {
         this.setState({
-          isUploading: false,
+          isUploading: false
         });
-        if (err.response.status === 403) {
+        if (err) {
           alert(
             `Your request for a signed URL failed with a status 403. Double check the CORS configuration and bucket policy in the README. You also will want to double check your AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY in your .env and ensure that they are the same as the ones that you created in the IAM dashboard. You may need to generate new keys\n${
               err.stack
@@ -72,27 +74,29 @@ class ImageUpload extends Component {
         <h1>{url}</h1>
         <img src={url} alt="" width="450px" />
 
-        {/* <Dropzone
+        <Dropzone
           onDropAccepted={this.getSignedRequest}
           style={{
-            position: 'relative',
+            position: "relative",
             width: 200,
             height: 200,
             borderWidth: 7,
             marginTop: 100,
-            borderColor: 'rgb(102, 102, 102)',
-            borderStyle: 'dashed',
+            borderColor: "rgb(102, 102, 102)",
+            borderStyle: "dashed",
             borderRadius: 5,
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            fontSize: 28,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: 28
           }}
           accept="image/*"
           multiple={false}
         >
-          <div>{isUploading ? <GridLoader /> : <p>Drop File or Click Here</p>}</div>
-        </Dropzone> */}
+          <div>
+            {isUploading ? <GridLoader /> : <p>Drop File or Click Here</p>}
+          </div>
+        </Dropzone>
       </div>
     );
   }
